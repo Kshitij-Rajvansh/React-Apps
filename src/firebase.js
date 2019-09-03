b63296e301e5167f1b6733e11c004103a49ccf89
kshraj <kshitij.raj@capgemini.com>
2019-09-03 17:34:59 +0530

    commiting from office

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: ''
+    apiKey: "AIzaSyByyod37Q4sZR3JmnOmYpLR1u4cRdb9gFI",
+    authDomain: "todoist-tut-fcd00.firebaseapp.com",
+    databaseURL: "https://todoist-tut-fcd00.firebaseio.com",
+    projectId: "todoist-tut-fcd00",
+    storageBucket: "todoist-tut-fcd00.appspot.com",
+    messagingSenderId: "515726339990",
+    appId: "1:515726339990:web:ccec4c9eb26ce92c"
 });
 
 export { firebaseConfig as firebase }
---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+import { collatedTasks } from "../constants"
+
+export const collatedTaskExist = (selectedProject) => {
+    collatedTaskExist.find( task => task.key === selectedProject);
+}
